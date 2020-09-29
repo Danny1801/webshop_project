ae0f4b8be9211c474f972f2ef3638c6e284b2ba6
FabioVanBreemen <fabio@oddity.nl>
2020-09-29 12:55:37 +0100

    footer

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 
-    echo "hello world1";
-    echo "test";
+    
 
 ?>
 <html>
-<head>
-
-</head>
-<body>
-
-</body>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    </head>
+    <body>
+        <?php include("header.php") ?>
+            <div>Website</div>
+        <?php include("footer.php") ?>
+    </body>
 </html>
