7e5f16e417683f5cbdb1537162ed7a47c74bbf65
Danny1801 <comesdanny@gmail.com>
2020-10-01 12:52:15 +0200

    header edit en login toegevoegd

--- header.php
+++ header.php
@@ -10,28 +10,45 @@
     </head>
     <body>
         <div class="header">
-            <div class="logo" >
-                <img src="./logo.png" class="rounded" alt="Cinque Terre">
+            <div class="container" >
+                <div class="logo">
+                    <!--<img src="./logo.png" class="rounded" alt="Cinque Terre" id="logo">-->
+                </div>
+                <div id="login">
+                    <a href="login.php">Login</a>
+                </div>
+                <div id="winkelwagen">
+                    <img src="images/cart.png" id="winkelwagen" href="cart.php">
+                </div>
             </div>
-            <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
 
+            <nav class="navbar navbar-expand-sm bg-dark navbar-dark" id="menubalk">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link 1</a>
+                <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link 2</a>
-                    </li>
-
-                    <!-- Dropdown -->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Dropdown link</a>
+                <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Componenten</a>
                     <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Link 1</a>
-                        <a class="dropdown-item" href="#">Link 2</a>
-                        <a class="dropdown-item" href="#">Link 3</a>
+                        <a class="dropdown-item" href="#">Processors</a>
+                        <a class="dropdown-item" href="#">videokaarten</a>
+                        <a class="dropdown-item" href="#">Ram</a>
+                        <a class="dropdown-item" href="#">Moederborden</a>
+                        <a class="dropdown-item" href="#">Koeling</a>
+                        <a class="dropdown-item" href="#">Voeding</a>
+                        <a class="dropdown-item" href="#">Opslag</a>
+                        <a class="dropdown-item" href="#">Behuizingen</a>
+                        <a class="dropdown-item" href="#">Accesiores</a>
                     </div>
+                </li>        
+                    <!-- Dropdown -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Over ons</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                    
                 </ul>
             </nav>
         <div>
